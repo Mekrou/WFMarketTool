6615d046fba011f2245c261124cccd3f659829ec
Mekrou <mills.ryan.011@gmail.com>
2023-11-20 13:10:05 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CommandLine;
+
+class Program
+{
+    public class Options
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
+        [Option('addlisting', Required = false, HelpText = "Add a listing to your account.")]
+    }
+
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+               .WithParsed<Options>(o =>
+               {
+                   if (o.Verbose)
+                   {
+                       Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                   }
+                   else
+                   {
+                       Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                       Console.WriteLine("Quick Start Example!");
+                   }
+               });
+    }
+}
